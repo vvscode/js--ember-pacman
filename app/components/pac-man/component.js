f90fb406553c16602ff454875e6a664bab117884
Vasil Vanchuk <v.vanchuk@tut.by>
2016-05-19 14:53:34 +0300

    Draw circle on start

--- app/components/pac-man/component.js
+++ app/components/pac-man/component.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+const {
+  on
+} = Ember;
+
 export default Ember.Component.extend({
+  onDinInsertElement: on('didInsertElement', function() {
+    this.drawCircle();
+  }),
+
+  drawCircle() {
+    const canvas = this.$('canvas')[0];
+    const ctx = canvas.getContext('2d');
+    const x = 50;
+    const y = 100;
+    const radius = 20;
+    ctx.fillStyle = '#000';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI*2, false);
+    ctx.closePath();
+    ctx.fill();
+  }
 });
